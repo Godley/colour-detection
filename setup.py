64b92057ddc0a0ba0454b0a951541a1bf72f174e
Godley <charlotte.godley@hotmail.co.uk>
2017-12-23 11:26:24 +0000

    chore: initial layout

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='colour-detection',
+    version='0.1',
+    packages=['colourdetection'],
+    url='https://github.com/ForestFighters/colour-detection',
+    license='MIT',
+    author='Charlotte Godley',
+    author_email='charlotte.godley@hotmail.co.uk',
+    description='python module for colour detection. Abstracted from opencv'
+)
