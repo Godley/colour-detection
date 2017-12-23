abf451c7d91eebb34c20462d9b660f59c0164df8
Godley <charlotte.godley@hotmail.co.uk>
2017-12-23 16:58:16 +0000

    chore: corrected url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='colour-detection',
     version='0.1',
     packages=['colourdetection'],
-    url='https://github.com/ForestFighters/colour-detection',
+    url='https://github.com/godley/colour-detection',
     license='MIT',
     author='Charlotte Godley',
     author_email='charlotte.godley@hotmail.co.uk',
